6e41c2ca1b712ae29c5912cd0822425f5d6070bb
Brice Colucci <bcolucci@wuha.io>
2016-07-19 16:35:44 +0200

    first step on server rendering with react

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
 
 import _ from 'lodash'
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
 import createStore from './createStore'
 import colors from './colors'
 import actions from './actions'
 import randomTurn from './random-turn'
+import Hello from './components/hello'
 
-const store = createStore(randomTurn)
+// hard-coded run to see results (random user answers)
+/*(() => {
 
-const states = [ store.getState() ]
+  const store = createStore(randomTurn)
+  const states = [ store.getState() ]
 
-let state;
-do {
-  const action = actions.play(_.sample(colors))
-  store.dispatch(action)
-  state = store.getState()
-  states.push(state)
-} while(!state.end);
+  let state;
+  do {
+    const action = actions.play(_.sample(colors))
+    store.dispatch(action)
+    state = store.getState()
+    states.push(state)
+  } while(!state.end);
 
-console.log(JSON.stringify(states, null, 2))
+  console.log(JSON.stringify(states, null, 2))
+
+})();*/
+
+console.log(ReactDOMServer.renderToString(<Hello/>));
---
+++ src/components/hello.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+export default class Hello extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <p>Hello</p>
+    )
+  }
+
+}
